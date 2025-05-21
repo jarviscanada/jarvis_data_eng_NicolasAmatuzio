1be977b7a09ba97a87b1a1fade35936fec67a707
Nicolas Amatuzio <nictuzio@gmail.com>
2025-05-21 06:01:22 +0000

    Implemented File reading and Directory listing

--- core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepImp.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepImp.java
@@ -1,11 +1,29 @@
 package ca.jrvs.apps.grep;
 
-import java.io.File;
-import java.io.IOException;
+import com.sun.org.slf4j.internal.Logger;
+import com.sun.org.slf4j.internal.LoggerFactory;
+import jdk.jfr.internal.tool.Main;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.io.File;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class JavaGrepImp implements JavaGrep {
+
+    final Logger logger = LoggerFactory.getLogger(JavaGrepImp.class);
+
+    private String regex;
+    private String outFile;
+    private String rootPath;
+
+    public static void Main (String [] args) {
+
+    }
 
     @Override
     public void process() throws IOException {
@@ -14,17 +32,34 @@ public class JavaGrepImp implements JavaGrep {
 
     @Override
     public List<File> listFiles(String rootDir) {
-        return Collections.emptyList();
+        File[] files = new File (rootDir).listFiles();
+        if (files == null) {
+            return Collections.emptyList();
+        }
+        return Arrays.asList(files);
     }
 
     @Override
     public List<String> readLines(File inputFile) {
-        return Collections.emptyList();
+        List<String> str = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFile))) {
+            String line;
+            while((line = reader.readLine()) != null) {
+                str.add(line);
+            }
+        } catch (IOException e) {
+            logger.debug(e.getMessage());
+            return null;
+        }
+        return str;
     }
 
     @Override
     public boolean containsPattern(String line) {
-        return false;
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(line);
+
+        return matcher.find();
     }
 
     @Override
